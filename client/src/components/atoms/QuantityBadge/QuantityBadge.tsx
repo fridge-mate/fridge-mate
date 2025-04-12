d88a9324fb69031b85be96bcf004de6d6dbfcdfd
中畠昇汰 <pandora.osk@gmail.com>
2025-04-12 17:13:32 +0800

    feat(ui): add QuantityBadge component and refine Label design

---
+++ client/src/components/atoms/QuantityBadge/QuantityBadge.tsx
@@ -0,0 +1,17 @@
+type QuantityBadgeProps = {
+  count:number
+}
+
+function QuantityBadge({count}:QuantityBadgeProps) {
+  return (
+    <div className="flex items-center space-x-1">
+      <button>▲</button>
+      <span className=" text-black text-xs font-semibold px-2 py-1">
+        Quantity: {count}
+      </span>
+      <button>▼</button> 
+    </div>
+  )
+}
+
+export default QuantityBadge
