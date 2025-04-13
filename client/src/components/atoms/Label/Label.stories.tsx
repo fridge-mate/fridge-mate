b1761fc3c8fc52abef836a0b72d072c0820a5374
中畠昇汰 <pandora.osk@gmail.com>
2025-04-13 11:37:59 +0800

    chore: format code

--- client/src/components/atoms/Label/Label.stories.tsx
+++ client/src/components/atoms/Label/Label.stories.tsx
@@ -1,29 +1,29 @@
-import { Meta,StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import Label from "@/components/atoms/Label/Label";
 
-const meta : Meta<typeof Label>={
-  title:"Atom/Label",
-  component:Label,
-  tags:["autodocs"]
-}
+const meta: Meta<typeof Label> = {
+	title: "Atom/Label",
+	component: Label,
+	tags: ["autodocs"],
+};
 export default meta;
 
 type Story = StoryObj<typeof Label>;
 
-export const Fruit:Story={
-  args:{
-    genreKey: 'fruit',
-  }
-}
+export const Fruit: Story = {
+	args: {
+		genreKey: "fruit",
+	},
+};
 
 export const Meat: Story = {
-  args: {
-    genreKey: 'meat',
-  },
+	args: {
+		genreKey: "meat",
+	},
 };
 
 export const Drink: Story = {
-  args: {
-    genreKey: 'drink',
-  },
+	args: {
+		genreKey: "drink",
+	},
 };
--- client/src/components/atoms/QuantityBadge/QuantityBadge.stories.tsx
+++ client/src/components/atoms/QuantityBadge/QuantityBadge.stories.tsx
@@ -1,17 +1,17 @@
-import { Meta,StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import QuantityBadge from "@/components/atoms/QuantityBadge/QuantityBadge";
 
-const meta:Meta<typeof QuantityBadge>={
-  title:'Atoms/QuantityBadge',
-  component:QuantityBadge,
-  tags:["autodocs"]
-}
+const meta: Meta<typeof QuantityBadge> = {
+	title: "Atoms/QuantityBadge",
+	component: QuantityBadge,
+	tags: ["autodocs"],
+};
 export default meta;
 
 type Story = StoryObj<typeof QuantityBadge>;
 
-export const  Default:Story={
-  args:{
-    count:3
-  }
-}
+export const Default: Story = {
+	args: {
+		count: 3,
+	},
+};
