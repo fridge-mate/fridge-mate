42474d1365343d586a22187509abd00c85565a20
中畠昇汰 <pandora.osk@gmail.com>
2025-04-16 21:29:27 +0800

    feat(ui): add ItemCard component and reuse GenreKey type from Label

---
+++ client/src/components/molecules/ItemCard/ItemCard.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ItemCard from "@/components/molecules/ItemCard/ItemCard";
+
+const meta: Meta<typeof ItemCard> = {
+	component: ItemCard,
+	title: "Molecules/ItemCard",
+	tags: ["autodocs"],
+};
+export default meta;
+type Story = StoryObj<typeof ItemCard>;
+
+export const Apple: Story = {
+	args: {
+		name: "リンゴ",
+		imageUrl: "/images/items/apple.webp",
+		daysLeft: -2,
+		category: "fruit",
+		quantity: 1,
+	},
+};
+
+export const Carrot: Story = {
+	args: {
+		name: "ニンジン",
+		imageUrl: "/images/items/carrot.webp",
+		daysLeft: 3,
+		category: "vegetable",
+		quantity: 2,
+	},
+};
---
+++ client/src/components/molecules/ItemCard/ItemCard.tsx
@@ -0,0 +1,50 @@
+import ExpirationBadge from "@/components/atoms/ExpirationBadge/ExpirationBadge";
+import Label from "@/components/atoms/Label/Label";
+import type { GenreKey } from "@/components/atoms/Label/Label";
+import QuantityBadge from "@/components/atoms/QuantityBadge/QuantityBadge";
+import DetailLabelWithButton from "@/components/atoms/DetailLabelWithButton/DetailLabelWithButton";
+type ItemCardProps = {
+	itemId: number;
+	name: string;
+	imageUrl: string; //stringでそのままpathをもらう設計でいいのか再検討
+	daysLeft: number;
+	category: GenreKey;
+	quantity: number;
+	onDetailClick: () => void;
+};
+
+const ItemCard: React.FC<ItemCardProps> = ({
+	itemId,
+	name,
+	imageUrl,
+	daysLeft,
+	category,
+	quantity,
+	onDetailClick,
+}) => {
+	return (
+		<div
+			className={`flex justify-between p-5 ${daysLeft < 0 ? "bg-red-100" : "bg-white"}`}
+		>
+			<div className="flex space-x-3">
+				<img src={`${imageUrl}`} alt="product" className="w-16 h-16" />
+				<div className="">
+					<ExpirationBadge daysLeft={daysLeft} />
+					<div className="flex space-x-3 items-center">
+						<p>{name}</p>
+						<Label genreKey={category} />
+					</div>
+				</div>
+			</div>
+			<div className="flex space-x-5">
+				<QuantityBadge count={quantity} />
+				<DetailLabelWithButton
+					itemId={itemId}
+					onClick={() => onDetailClick()}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default ItemCard;
