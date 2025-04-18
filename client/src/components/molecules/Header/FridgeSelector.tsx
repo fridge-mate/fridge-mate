628611f23d1ffef7cec3071481f61c639356d5fe
中畠昇汰 <pandora.osk@gmail.com>
2025-04-19 01:24:41 +0800

    chore(ui): setup Header module structure

---
+++ client/src/components/molecules/Header/FridgeSelector.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+//FridgeSelectorのstateによってhomeで表示するlistも変更するため今後reduxで管理するのがいい？
+
+const FridgeSelector = () => {
+	return <div>FridgeSelector</div>;
+};
+
+export default FridgeSelector;
