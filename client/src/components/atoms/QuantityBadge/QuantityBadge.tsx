804237427c606b745a3d272c67732623d24f04fe
kyo8300 <kametyou777@gmail.com>
2025-04-18 16:10:05 +0800

    fix: lint errors

--- client/src/components/atoms/QuantityBadge/QuantityBadge.tsx
+++ client/src/components/atoms/QuantityBadge/QuantityBadge.tsx
@@ -5,11 +5,11 @@ type QuantityBadgeProps = {
 function QuantityBadge({ count }: QuantityBadgeProps) {
 	return (
 		<div className="flex items-center space-x-1">
-			<button>▲</button>
+			<button type="button">▲</button>
 			<span className=" text-black text-xs font-semibold px-2 py-1">
 				Quantity: {count}
 			</span>
-			<button>▼</button>
+			<button type="button">▼</button>
 		</div>
 	);
 }
