71573b17f31faa7d553b2da3341be97c07ee87c8
中畠昇汰 <pandora.osk@gmail.com>
2025-04-19 17:33:01 +0800

    style(select): apply Biome formatting

---
+++ client/src/components/molecules/Toolbar/Toolbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "@/components/atoms/SerchBar/SearchBar";
+
+const Toolbar = () => {
+	return (
+		<div>
+			<div>
+				<SearchBar />
+			</div>
+		</div>
+	);
+};
+
+export default Toolbar;
