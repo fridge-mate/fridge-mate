f3805f9f4a156bd7a8f6921a0090f75c87e98cdd
中畠昇汰 <pandora.osk@gmail.com>
2025-04-24 06:51:22 +0800

    feat(items): implement category & view mode filtering with Jotai and custom hook

--- client/src/features/items/components/Itemlist/Itemlist.tsx
+++ client/src/features/items/components/Itemlist/Itemlist.tsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
-import { useFetchItemsQuery } from "@/features/items/hooks/useFetchItems";
+import { useFilteredItems } from "@/features/items/hooks/useFilteredItems";
 import ItemCard, {
 	type ItemCardProps,
 } from "@/features/items/components/ItemCard/ItemCard";
 import DetailModal from "@/features/items/components/modal/DetailModal";
-import { useAtomValue } from "jotai";
-import { selectedCategoryAtom } from "@/store/category";
 
 const ItemList = () => {
-	// const { data } = useFetchItems();
-	const { data = [], isLoading, isError } = useFetchItemsQuery();
-	const selectedCategory = useAtomValue(selectedCategoryAtom);
+	const { data = [], isLoading, isError } = useFilteredItems();
 	const [selectedItem, setSelectedItem] = useState<ItemCardProps | null>(null);
 
 	const handleOpenModal = (item: ItemCardProps) => {
@@ -21,17 +17,15 @@ const ItemList = () => {
 		setSelectedItem(null);
 	};
 
-	const filteredItems =
-		selectedCategory === "all"
-			? data
-			: data.filter((data) => data.category === selectedCategory);
-
 	if (isLoading) return <p>is Loading...</p>;
 	if (isError) return <p>發生錯誤，請重新載入頁面。</p>;
+	if (data.length === 0) {
+		return <p>該当するアイテムがありません。</p>;
+	}
 
 	return (
 		<div>
-			{filteredItems.map((item, index) => (
+			{data.map((item, index) => (
 				<ItemCard
 					key={item.itemId}
 					{...item}
