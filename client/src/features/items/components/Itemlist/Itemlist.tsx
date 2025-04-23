2280e51b989a6f99e2fa800a411f593401992831
中畠昇汰 <pandora.osk@gmail.com>
2025-04-24 03:01:38 +0800

    feat(items): implement category-based filtering with Jotai and display cached item list

--- client/src/features/items/components/Itemlist/Itemlist.tsx
+++ client/src/features/items/components/Itemlist/Itemlist.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import { useFetchItems } from "@/features/items/hooks/useFetchItems";
+import { useFetchItemsQuery } from "@/features/items/hooks/useFetchItems";
 import ItemCard, {
 	type ItemCardProps,
 } from "@/features/items/components/ItemCard/ItemCard";
 import DetailModal from "@/features/items/components/modal/DetailModal";
+import { useAtomValue } from "jotai";
+import { selectedCategoryAtom } from "@/store/category";
 
 const ItemList = () => {
-	const { data } = useFetchItems();
+	// const { data } = useFetchItems();
+	const { data = [], isLoading, isError } = useFetchItemsQuery();
+	const selectedCategory = useAtomValue(selectedCategoryAtom);
 	const [selectedItem, setSelectedItem] = useState<ItemCardProps | null>(null);
 
 	const handleOpenModal = (item: ItemCardProps) => {
@@ -17,9 +21,17 @@ const ItemList = () => {
 		setSelectedItem(null);
 	};
 
+	const filteredItems =
+		selectedCategory === "all"
+			? data
+			: data.filter((data) => data.category === selectedCategory);
+
+	if (isLoading) return <p>is Loading...</p>;
+	if (isError) return <p>發生錯誤，請重新載入頁面。</p>;
+
 	return (
 		<div>
-			{data.map((item, index) => (
+			{filteredItems.map((item, index) => (
 				<ItemCard
 					key={item.itemId}
 					{...item}
