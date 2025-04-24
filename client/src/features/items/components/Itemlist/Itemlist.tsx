30047d5fe45b3c44a3fb65bef17d6756fc695d2e
中畠昇汰 <pandora.osk@gmail.com>
2025-04-25 06:26:46 +0800

    chore: move provider and providers files to new directory structure

--- client/src/features/items/components/Itemlist/Itemlist.tsx
+++ client/src/features/items/components/Itemlist/Itemlist.tsx
@@ -1,14 +1,9 @@
 import { useState } from "react";
-<<<<<<< HEAD
 import { useFilteredItems } from "@/features/items/hooks/useFilteredItems";
-=======
-import { useFetchItems } from "@/features/items/hooks/useFetchItems";
->>>>>>> 93dd262 (feat(modal): implement ItemDetailModal component to display item info)
 import ItemCard, {
 	type ItemCardProps,
 } from "@/features/items/components/ItemCard/ItemCard";
 import DetailModal from "@/features/items/components/modal/DetailModal";
-<<<<<<< HEAD
 
 const ItemList = () => {
 	const { data = [], isLoading, isError } = useFilteredItems();
@@ -27,22 +22,6 @@ const ItemList = () => {
 	if (data.length === 0) {
 		return <p>沒有符合條件的項目。</p>;
 	}
-
-=======
-
-const ItemList = () => {
-	const { data } = useFetchItems();
-	const [selectedItem, setSelectedItem] = useState<ItemCardProps | null>(null);
-
-	const handleOpenModal = (item: ItemCardProps) => {
-		setSelectedItem(item);
-	};
-
-	const handleCloseModal = () => {
-		setSelectedItem(null);
-	};
-
->>>>>>> 93dd262 (feat(modal): implement ItemDetailModal component to display item info)
 	return (
 		<div>
 			{data.map((item, index) => (
